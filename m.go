19526903d56cf3e7bf94ee8ac4305c7e56d8b70c
Ganesh K <ganeshk.work@gmail.com>
2021-04-21 08:15:58 +0530

    update lexer

--- m.go
+++ m.go
@@ -6,25 +6,31 @@ import (
 )
 
 const (
-	TokenLeftBrace = iota
-	TokenRightBrace
-	TokenRightParenthesis
-	TokenLeftParenthesis
-	TokenPlus
-	TokenEqual
-	TokenMinus
-	TokenSemiColon
+	TokenLeftBrace        = "Left_Brace"
+	TokenRightBrace       = "Right_Brace"
+	TokenRightParenthesis = "Right_Parenthesis"
+	TokenLeftParenthesis  = "Left_Parenthesis"
+	TokenPlus             = "Plus"
+	TokenEqual            = "Equal"
+	TokenEqualEqual       = "Equal_Equal"
+	TokenMinus            = "Minus"
+	TokenSemiColon        = "Semicolon"
+	TokenLessThan         = "Less_Than"
+	TokenGreaterThan      = "Greater_Than"
+	TokenGreaterOrEqual   = "Greater_Or_Equal"
+	TokenLessOrEqual      = "Less_Or_Equal"
+	TokenBang             = "Bang"
+	TokenBangEqual        = "Bang_Equal"
 )
 
 type token struct {
 	literal   string
-	tokenType int
+	tokenType string
 	line      int
 }
 
 func main() {
-	lexemes := lexer(rawSource())
-	fmt.Print(lexemes)
+	fmt.Println(lexer(rawSource()))
 }
 
 func rawSource() string {
@@ -40,84 +46,156 @@ func rawSource() string {
 }
 
 func lexer(src string) *[]token {
+	var tokens []token
 	line := 1
+	whitespace := 0
 
-	var lexeme []token
+	for i := 0; i < len(src); i++ {
+		// Ignore comment for now.
+		switch string(src[i]) {
 
-	for _, c := range src {
-		if c == '\n' {
+		case "\n":
 			line++
-			continue
-		}
+			break
 
-		if c == ' ' || c == '\t' {
-			continue
-		}
+		case " ":
+			whitespace++
+			for string(src[i+1]) == " " {
+				i++
+				whitespace++
+			}
+			break
 
-		if c == '(' {
-			lexeme = append(lexeme, token{
-				literal:   string(c),
+		case "(":
+			tokens = append(tokens, token{
+				literal:   string(src[i]),
 				tokenType: TokenLeftParenthesis,
 				line:      line,
 			})
-		}
+			break
 
-		if c == ')' {
-			lexeme = append(lexeme, token{
-				literal:   string(c),
+		case "!":
+			if string(src[i+1]) == "=" {
+				tokens = append(tokens, token{
+					literal:   string(src[i]) + string(src[i+1]),
+					tokenType: TokenBangEqual,
+					line:      line,
+				})
+				i++
+			} else {
+				tokens = append(tokens, token{
+					literal:   string(src[i]),
+					tokenType: TokenBang,
+					line:      line,
+				})
+			}
+			break
+
+		case ")":
+			tokens = append(tokens, token{
+				literal:   string(src[i]),
 				tokenType: TokenRightParenthesis,
 				line:      line,
 			})
-		}
+			break
 
-		if c == '{' {
-			lexeme = append(lexeme, token{
-				literal:   string(c),
+		case "{":
+			tokens = append(tokens, token{
+				literal:   string(src[i]),
 				tokenType: TokenLeftBrace,
 				line:      line,
 			})
-		}
+			break
 
-		if c == '}' {
-			lexeme = append(lexeme, token{
-				literal:   string(c),
+		case "}":
+			tokens = append(tokens, token{
+				literal:   string(src[i]),
 				tokenType: TokenRightBrace,
 				line:      line,
 			})
-		}
+			break
 
-		if c == '+' {
-			lexeme = append(lexeme, token{
-				literal:   string(c),
-				tokenType: TokenPlus,
-				line:      line,
-			})
-		}
+		case "=":
+			if string(src[i+1]) == "=" {
+				tokens = append(tokens, token{
+					literal:   string(src[i]) + string(src[i+1]),
+					tokenType: TokenEqualEqual,
+					line:      line,
+				})
+				i++
+			} else {
+				tokens = append(tokens, token{
+					literal:   string(src[i]),
+					tokenType: TokenEqual,
+					line:      line,
+				})
+			}
+			break
 
-		if c == '=' {
-			lexeme = append(lexeme, token{
-				literal:   string(c),
-				tokenType: TokenEqual,
-				line:      line,
-			})
-		}
-
-		if c == '-' {
-			lexeme = append(lexeme, token{
-				literal:   string(c),
-				tokenType: TokenMinus,
-				line:      line,
-			})
-		}
-
-		if c == ';' {
-			lexeme = append(lexeme, token{
-				literal:   string(c),
+		case ";":
+			tokens = append(tokens, token{
+				literal:   string(src[i]),
 				tokenType: TokenSemiColon,
 				line:      line,
 			})
+			break
+
+		case "+":
+			tokens = append(tokens, token{
+				literal:   string(src[i]),
+				tokenType: TokenPlus,
+				line:      line,
+			})
+			break
+
+		case "-":
+			tokens = append(tokens, token{
+				literal:   string(src[i]),
+				tokenType: TokenMinus,
+				line:      line,
+			})
+			break
+
+		case ">":
+			if string(src[i+1]) == "=" {
+				tokens = append(tokens, token{
+					literal:   string(src[i]) + string(src[i+1]),
+					tokenType: TokenGreaterOrEqual,
+					line:      line,
+				})
+				i++
+			} else {
+				tokens = append(tokens, token{
+					literal:   string(src[i]),
+					tokenType: TokenGreaterThan,
+					line:      line,
+				})
+			}
+			break
+
+		case "<":
+			if string(src[i+1]) == "=" {
+				tokens = append(tokens, token{
+					literal:   string(src[i]) + string(src[i+1]),
+					tokenType: TokenLessOrEqual,
+					line:      line,
+				})
+				i++
+			} else {
+				tokens = append(tokens, token{
+					literal:   string(src[i]),
+					tokenType: TokenLessThan,
+					line:      line,
+				})
+			}
+			break
+
+		default:
+
 		}
 	}
 
-	return &lexeme
+	fmt.Println("Total whitespace ", whitespace)
+	fmt.Println("Total lines ", line)
+	return &tokens
 }
